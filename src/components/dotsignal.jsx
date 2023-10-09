1ef37e699dc7519d340b4e75fe7934bebbdc4f0e
utkarshtri03 <utkarshtri03@gmail.com>
2023-10-10 00:19:30 +0530

    dotsignal

--- src/components/dotsignal.jsx
+++ src/components/dotsignal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const Dotsignal = ({ status }) => {
-  const dotClass = status === "Shipped" ? "bg-gray-500" : "bg-green-500";
+  const dotClass = status === "Processing" ? "bg-gray-500" : "bg-green-500";
   return <div className={`w-[10px] h-[10px] rounded-full ${dotClass}`}></div>;
 };
